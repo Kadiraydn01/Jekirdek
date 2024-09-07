08acfc6438394a91d413751ef530914f8d49fe69
Kadir Aydın <kadiraydn01@gmail.com>
2024-09-07 16:52:57 +0300

    Add User Entity

---
+++ project/src/main/java/com/jekirdek/project/Entity/User.java
@@ -0,0 +1,31 @@
+package com.jekirdek.project.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "role")
+    private Role role;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
